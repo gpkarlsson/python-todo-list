78bfef81723fdbb16fce1cf084b8878000d096aa
Gordon Karlsson <gpkarlsson72@gmail.com>
2024-08-15 17:44:10 -0500

    working on updating menu options, input validation, exit function, addTask function and more

--- todo.py
+++ todo.py
@@ -14,41 +14,62 @@
 #ENHANCEMENTS
     # Remove item from list
     #
-TO_DO_LIST = []
-MENU_OPTIONS = ["View Tasks", "Add New Task", "Complete Task", "Exit"]
-    # Display Menu
-def displayMenu():
-    
-    # Get User Input
+
+  # Get User Input
     # Handle Each Option
     # Loop Until Exit
-    return
+TO_DO_LIST = []
+MENU_OPTIONS = ["1. View Tasks", "2. Add New Task", "3. Complete Task", "4. Exit"]
+    
+    # Display Menu
+def displayMenu():
+    print(MENU_OPTIONS)
+
 def addTask():
     #add task logic here
     newTask = input("Please add a task: ")
     TO_DO_LIST.append(newTask)
-    return
-
+    print("Task Added")
+    print(TO_DO_LIST)
+    main()
+    
 def viewList():
-    #display all tasks
-    i = 0
-    while i < len(TO_DO_LIST):
-        print (TO_DO_LIST[i])
-        i = i + 1
-    return
+    # display all tasks
+    # i = 0
+    # while i < len(TO_DO_LIST):
+    #     print(TO_DO_LIST[i])
+    #     i = i + 1
+    #     break
+    # return
+    print(MENU_OPTIONS)
+    for newMenu in MENU_OPTIONS:
+        print(newMenu, end=',')
+    main()
+
 def completeTask():
     #complete individual task
     removeTask = input("Please select which task to complete: ")
     TO_DO_LIST.remove(removeTask)
+    main()
+
+def exit():
+     print("later nerd")
+     return
+
 
 def main():
-        userinput = input("Please Select Task: ").lower()
-        if userinput == "view":
-            viewList()
-        elif userinput == "add":
-            return addTask()
-        # elif:
-        #     userinput == "complete" 
-        #     completeTask()
+        displayMenu() 
+        userInput = int(input("Please Select Task: "))
+        if userInput < 0:
+            print("Please enter a number between 1 and 4")
+        elif userInput > 4:
+            print("Please enter a number between 1 and 4")
+            if userInput == 1:
+                viewList()
+            elif userInput == 2:
+                addTask()
+            elif userInput == 3:
+                completeTask()
         
+
 main()
